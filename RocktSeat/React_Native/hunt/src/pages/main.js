afe40f0fd68584e714daa1c646af457cfd729898
dzampa <diego_zamparo@hotmail.com>
2019-06-14 10:08:38 -0300

    add react_Native

---
+++ RocktSeat/React_Native/hunt/src/pages/main.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+import api from '../services/api';
+
+export default class Main extends Component {
+    static navigationOptions = {
+        title: "JSHunt"
+    };
+
+    state = {
+        docs: [],
+    };
+
+    componentDidMount(){
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('/products');
+
+        const { docs } = response.data;
+
+        this.setState({ docs });
+    };
+
+    render(){
+        return(
+            <View>
+                <Text>Página Main</Text>
+                {this.state.docs.map(product => (
+                    <Text key={product._id}>{product.title}</Text>
+                ))}
+            </View>
+        );
+    }
+}
